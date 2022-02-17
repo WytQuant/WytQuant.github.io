fc7a92ea59615beb0c050b29b4b6a49b6a96797e
WytQuant <wittawas.kns@gmail.com>
2022-02-17 14:04:36 +0700

    intro to js

---
+++ intro-to-js/wittawas.js
@@ -0,0 +1,14 @@
+function askMyName() {
+    const name = prompt('What is your name ?') 
+ 
+    if (name != null) {
+       document.getElementById("myname").innerHTML = "My name is " + name;
+       document.querySelector('#myname').style = 'background: linear-gradient(90deg, #54BAB9 0%, #E9DAC1 60%, #F7ECDE 95%);'
+       document.querySelector('.bg').style = 'background: linear-gradient(135deg, #FFBBBB 0%, #FFE4C0 60%, #F0FFC2 95%);'
+    }
+ }
+ 
+ function helloWorld() {
+ const h1message = document.getElementById('myname').innerHTML;
+    alert(h1message + ' : อยากเรียนอินทิเกรต จะได้หาพิ้นที่ว่างในหัวใจเธอ')
+ }
