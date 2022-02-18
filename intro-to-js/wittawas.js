fa88833bde5af84d7aaaeea0c425dfcbe8138b8e
WytQuant <wittawas.kns@gmail.com>
2022-02-18 14:17:54 +0700

    Added new fearture

--- intro-to-js/wittawas.js
+++ intro-to-js/wittawas.js
@@ -12,3 +12,35 @@ function askMyName() {
  const h1message = document.getElementById('myname').innerHTML;
     alert(h1message + ' : ไม่ต้องเขียนคิ้วให้คม พี่มองนมไม่ได้มองหน้า')
  }
+
+
+ function calAge() {
+
+   while (true) {
+      let userBirthYear = +prompt('What is your birth year ?');
+      let thisYear = 2022;
+      let calAge = thisYear - userBirthYear;
+      
+      if (isNaN(calAge) || !userBirthYear) {
+         alert('Are you God ??, Please fill out your birth year again');
+         continue;
+        } else {
+         alert(`Unfortunately, you are in hell for ${calAge} years`);
+         break;
+      }
+        
+   }
+ 
+
+    document.querySelector('.bg').style = 'background: linear-gradient(135deg, #FFBBBB 0%, #FFE4C0 60%, #F0FFC2 95%);'
+} 
+
+
+ function loadProfile() {
+    let myName = 'Wittawas';
+    let myProvince = 'Chainat';
+
+    document.querySelector('#my-name').innerHTML = myName;
+    document.querySelector('#my-province').innerHTML = myProvince;
+    document.querySelector('.bg').style = 'background: linear-gradient(135deg, #FFBBBB 0%, #FFE4C0 60%, #F0FFC2 95%);'
+ }
